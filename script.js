d836b6610c54cf14b12ee5bc6aa2c3c2eb886026
Erwan Hivin <erwan_hivin@ens.univ-artois.fr>
2025-03-04 22:53:53 +0100

    fix: mobile css

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ collapsibles.forEach(button => {
     button.addEventListener('click', function() {
         const content = button.closest('.skill-collapse').querySelector('.content');
         
+        button.classList.toggle('active');
+
         if (content.style.display === 'none' || content.style.display === '') {
             content.style.display = 'flex';
         } else {
